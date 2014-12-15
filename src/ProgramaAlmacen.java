27078ca6a65b5a36ca89c2630e79f71880f9d5e4
zubiri <zubiri@Ubuntu-12-04.(none)>
2014-12-15 12:15:18 +0100

    3.12

--- src/ProgramaAlmacen.java
+++ src/ProgramaAlmacen.java
@@ -48,31 +48,7 @@ public class ProgramaAlmacen {
 				dist.setPersonaContacto(contacto);
 
 				listaDistribuidores.add(dist);
-				/*
-				String [] atribDireccion = atributos[2].split("\\*");
-				System.out.println("\tDireccion:");
-
-				System.out.println("\t\tPueblo: "+atribDireccion[0]);
-				System.out.println();
-				System.out.println("\t\tCalle: "+atribDireccion[1]);
-				System.out.println();
-				System.out.println("\t\tNumero: "+atribDireccion[2]);
-				System.out.println();
-				System.out.println("\t\tCodigo Postal: "+atribDireccion[3]);
-				System.out.println();
-
-				String [] atribContacto = atributos[3].split("\\*");
-				System.out.println("\tContacto:");
-				System.out.println("\t\tNombre: "+atribContacto[0]);
-				System.out.println();
-				System.out.println("\t\tTelefono: "+atribContacto[1]);
-				System.out.println();
-				System.out.println("\t\tE-mail: "+atribContacto[2]);
-				System.out.println();
-				
-
-				
-				cont++;*/
+			
 			}
 			leerFichero.close();
 			for (int i=0; i<listaDistribuidores.size(); i++) {
@@ -94,6 +70,79 @@ public class ProgramaAlmacen {
 				System.out.println("\t\tTelefono: " + contac1.getTlf());
 				System.out.println("\t\te-mail: " + contac1.getEmail());
 				
+			}
+
+			// Pedir 2 manzanas
+			ArrayList <Manzana> manzanas = new ArrayList <Manzana>();
+			Scanner tecla = new Scanner(System.in);
+			/*System.out.println("Cuantas lechugas quieres insertar?: ");
+			int opcion = tecla.nextInt();*/
+			//crear
+			for(int i=0; i<2; i++) {
+				System.out.println("Manzana"+(i+1)+":");
+				Manzana m1 = new Manzana();
+				System.out.println("Tipo de Manzana: ");
+				m1.setTipoLechuga(tecla.next());
+				System.out.println("Procedencia: ");
+				m1.setProcedencia(tecla.next());
+				System.out.println("Color: ");
+				m1.setColor(tecla.next());
+				System.out.println("Precio €/kg: ");
+				m1.setEurosKilo(tecla.nextDouble());
+				System.out.println("Distribuidor: ");
+				Distribuidor d1 = new Distribuidor();
+				d1.setNombre(tecla.next());
+				for(int j=0; j<listaDistribuidores.size(); j++) {
+					if((listaDistribuidores.get(j).getNombre()).equalsIgnoreCase(d1.getNombre())) {
+						m1.setDistribuidor(listaDistribuidores.get(j));
+					}
+				}
+				manzanas.add(m1);
+			}
+			// Pedir 2 lechugas
+			ArrayList <Lechuga> lechugas = new ArrayList<Lechuga>();
+			//crear
+			for(int i=0; i<2; i++) {
+				System.out.println("Lechuga "+(i+1)+":");
+				Lechuga l1 = new Lechuga();
+				System.out.println("Tipo de lechuga: ");
+				l1.setTipoLechuga(tecla.next());
+				System.out.println("Procedencia: ");
+				l1.setProcedencia(tecla.next());
+				System.out.println("Color: ");
+				l1.setColor(tecla.next());
+				System.out.println("Precio €/Ud: ");
+				l1.setEurosUnidad(tecla.nextDouble());
+				System.out.println("Distribuidor: ");
+				Distribuidor d1 = new Distribuidor();
+				d1.setNombre(tecla.next());
+				for(int j=0; j<listaDistribuidores.size(); j++) {
+					if((listaDistribuidores.get(j).getNombre()).equalsIgnoreCase(distribuidor.getNombre())) {
+						l1.setDistribuidor(listaDistribuidores.get(j));
+					}
+				}
+				lechugas.add(l1);
+			}
+			// Mostrar lechugas
+			System.out.println("Lechugas:");
+			for (int i=0; i<lechugas.size(); i++){
+				System.out.println("Tipo: "+lechugas.get(i).getTipoLechuga());
+				System.out.println("Procedencia: "+lechugas.get(i).getProcediencia());
+				System.out.println("Color: "+lechugas.get(i).getColor());
+				System.out.println("Precio €/Ud: "+lechugas.get(i).getEurosUnidad());
+				Distribuidor dis1 = lechugas.get(i).getDistribuidor();
+				System.out.println("Nombre del distribuidor: "+dis1.getNombre());
+				System.out.println("CIF: "+dis1.getCIF());
+				System.out.println("Direccion: ");
+				System.out.println("\tPueblo: "+(d1.getDireccion()).getPueblo());
+				System.out.println("\tCalle: "+(d1.getDireccion()).getCalle());
+				System.out.println("\tNumero: "+(d1.getDireccion()).getNumero());
+				System.out.println("\tCalle: "+(d1.getDireccion()).getCP());
+				System.out.println("Contacto: ");
+				System.out.println("\tNombre: "+(d1.getPersonaContacto()).getNombreContacto());
+				System.out.println("\tTelefono: "+(d1.getPersonaContacto()).getTlf());
+				System.out.println("\te-mail: "+(d1.getPersonaContacto()).getEmail());
+		
 			}
 
 		} catch (Exception e) {
